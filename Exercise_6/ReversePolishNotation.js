8f63c9b17d8396b00303830eaf964fa34b519b57
Łukasz Jędrzejewski <kdmrulez@gmail.com>
2017-08-28 12:41:57 +0200

    Changes after code review and 3rd part of task done

--- Exercise_6/ReversePolishNotation.js
+++ Exercise_6/ReversePolishNotation.js
@@ -1,44 +1,43 @@
-// const process = require('process');
-// console.log(calculate(process.argv[2]));
-
-function calculate(argument,extraOperators) {
-    const invalidArgumentError = new Error('The argument ' + argument + ' is invalid');
+function calculate(argument, extraOperators) {
+    if (!argument) throw new Error(argument + " is not allowed");
     const signs = argument.split(' ');
-    const stack=[];
+    const operators = {
+        "*": (firstValue, secondValue) => firstValue * secondValue,
+        "+": (firstValue, secondValue) => firstValue + secondValue,
+        "-": (firstValue, secondValue) => {
+            return secondValue - firstValue
+        }
+    };
+
+
+    if (extraOperators) {
+        Object.assign(operators, extraOperators);
+    }
 
     function isNumber(sign) {
         return !isNaN(parseInt(sign)) && isFinite(sign);
     }
 
     function isValidOperator(sign) {
-        const isValidOperatorRegex = new RegExp(/^[+\-*]$/);
-        return isValidOperatorRegex.test(sign);
+        return operators.hasOwnProperty(sign);
     }
 
-
-    signs.reduce((stack,sign) => {
+    const resultStack = signs.reduce((stack, sign) => {
         if (isNumber(sign)) {
             stack.push(parseInt(sign));
         } else if (isValidOperator(sign)) {
-            if (stack.length < 2) throw invalidArgumentError;
+            if (stack.length < 2)throw new Error('Number of operators is not correct');
             const firstValue = stack.pop();
             const secondValue = stack.pop();
-            switch (sign) {
-                case "+":
-                    stack.push(firstValue + secondValue);
-                    break;
-                case "-":
-                    stack.push(secondValue - firstValue);
-                    break;
-                case "*":
-                    stack.push(firstValue * secondValue);
-                    break;
-            }
-        } else throw invalidArgumentError;
-      return stack;
-    },stack);
-    if (stack.length !== 1) throw invalidArgumentError;
-    return stack.pop();
+            stack.push(operators[sign](firstValue, secondValue));
+        } else {
+            throw new Error(sign + ' is not valid argument element')
+        }
+        return stack;
+    }, []);
+
+    if (resultStack.length !== 1) throw new Error('Number of digits  is not correct');
+    return resultStack.pop();
 }
 
 module.exports.calculate = calculate;
---
+++ Exercise_6/ReversePolishNotationSpec.js
@@ -0,0 +1,95 @@
+const assert = require('assert');
+const chai = require('chai');
+const calculate = require('./ReversePolishNotation').calculate;
+
+describe('ReversePolishNotation', () => {
+
+    describe("Check single digit", () => {
+        it('Should return the same digit ', () => {
+            assert.equal(calculate('0'), 0);
+        })
+    })
+
+    describe("Single operations", () => {
+        it('Should return the sum', () => {
+            assert.equal(calculate('2 3 4 5 6 + + + +'), 20);
+        })
+        it('Should return the subtraction', () => {
+            assert.equal(calculate('4 2 - 3 4 - -'), 3);
+        })
+        it('Should return the multiplication', () => {
+            assert.equal(calculate('12 3 * 4 *'), 144);
+        })
+    })
+
+    describe("Check mixed operations ", () => {
+        it('+ - *', () => {
+            assert.equal(calculate('5 1 2 + 4 * 3 - +'), 14);
+        })
+        it('+ *', () => {
+            assert.equal(calculate('4 2 5 * + 1 3 2 * + +'), 21);
+        })
+        it('+ -', () => {
+            assert.equal(calculate('2 3 + 4 12 1 3 - - + +'), 23);
+        })
+        it('* -', () => {
+            assert.equal(calculate('5 6 2 1 - * *'), 30);
+        })
+    })
+
+    describe("Check extra operators", () => {
+        it("add / to operators", () => {
+            assert.equal(calculate('4 2 5 * + 1 3 2 * + /',
+                {
+                    '/': (firstValue, secondValue) => {
+                        return secondValue / firstValue
+                    }
+                }), 2)
+        })
+
+        it("add * to operators", () => {
+            assert.equal(calculate('4 2 %',
+                {
+                    '%': (firstValue, secondValue) => {
+                        return secondValue % firstValue
+                    }
+                }), 0)
+        })
+    })
+
+    describe("throw error on invalid argument", () => {
+        it("should throw error with message 'null is not allowed' for null argument", () => {
+            chai.expect(() => calculate(null)).to.throw("null is not allowed");
+        })
+        it("should throw error with message 'undefined is not allowed' for undefined argument", () => {
+            chai.expect(() => calculate(undefined)).to.throw("undefined is not allowed");
+        })
+        it("should throw error with message 'is not allowed' for empty argument", () => {
+            chai.expect(() => calculate('')).to.throw("is not allowed");
+        })
+        it("should throw error with message 'is not valid argument element' for empty spaces", () => {
+            chai.expect(() => calculate('   ')).to.throw("is not valid argument element");
+        })
+        it("should throw error with message 'is not valid argument element'", () => {
+            chai.expect(() => calculate('2 3a +')).to.throw("is not valid argument element");
+        })
+        it("should throw error with message 'Number of operators is not correct'", () => {
+            chai.expect(() => calculate('2 3 4 + + -')).to.throw("Number of operators is not correct");
+        })
+        it("should throw error with message 'Number of digits is not correct'", () => {
+            chai.expect(() => calculate('2 3 4 + + 8')).to.throw("Number of digits  is not correct");
+        })
+    })
+
+    describe("handling invalid extra operators", () => {
+
+        it("should throw error with message 'A is not valid argument element' ",()=>{
+            chai.expect(()=>calculate('2 3 A +','A')).to.throw("A is not valid argument element");
+        })
+
+        it("should return correct value", ()=>{
+            assert.equal(calculate('2 3 +',{'a':''}),5)
+        })
+    })
+});
+
