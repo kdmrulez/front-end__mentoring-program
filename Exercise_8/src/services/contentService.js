ee83f94648d63503d126e595891f0acc482b7ae5
Łukasz Jędrzejewski <kdmrulez@gmail.com>
2017-10-02 15:08:20 +0200

    Solution to 8th task, related with handling browser history

---
+++ Exercise_8/src/services/contentService.js
@@ -0,0 +1,15 @@
+import api from '../api';
+import setContent from '../content';
+
+const contentService = {
+  setHomeContent: () => {
+    api.getHome()
+      .then((data) => { setContent(data); });
+  },
+  setSignInContent: () => {
+    api.getSignIn()
+      .then((data) => { setContent(data); });
+  },
+};
+
+export default contentService;
