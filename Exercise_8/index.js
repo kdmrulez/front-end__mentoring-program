02a10e2daaf2b17a007ba844ba8025fe5ed76ba9
Łukasz Jędrzejewski <kdmrulez@gmail.com>
2017-10-02 15:08:20 +0200

    Solution to 8th task, related with handling browser history

---
+++ Exercise_8/index.js
@@ -0,0 +1,22 @@
+import './css/style.scss';
+import contentService from './src/services/contentService';
+import historyService from './src/services/historyService';
+
+/* global window:true */
+window.onload = () => {
+  contentService.setHomeContent();
+};
+
+window.document.querySelectorAll('.sign-in').forEach((button) => {
+  button.addEventListener('click', () => {
+    historyService.addToHistory('/sign-in');
+    contentService.setSignInContent();
+  }, false);
+});
+
+window.onpopstate = () => {
+  const singInUrl = '/sign-in';
+  const currentUrl = window.location.pathname;
+  if (currentUrl !== singInUrl) contentService.setHomeContent();
+  else contentService.setSignInContent();
+};
---
+++ Exercise_8/src/content.js
@@ -0,0 +1,6 @@
+/* global window:true */
+const setContent = (data) => {
+  window.document.getElementById('content').innerHTML = data;
+};
+
+export default setContent;
